b603c0c9805b657cbc5767bcc0bd16c1a867e062
guffy1O <guffy1079@gmail.com>
2025-11-21 06:33:25 +0300

    2 task

---
+++ src/Box.java
@@ -0,0 +1,11 @@
+public class Box<T> {
+    private T item;
+
+    public Box(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,26 @@ public class Main {
         System.out.println("После перестановки: " + Arrays.toString(numbers));
     }
 
+    public static void task2() {
+        Box<String> stringBox = new Box<>("Привет");
+        Box<Integer> integerBox = new Box<>(10);
+        Box<Float> floatBox = new Box<>(3.14f);
+
+        double sum = calculateSum(integerBox.getItem(), floatBox.getItem());
+        System.out.println("Общая сумма чисел: " + sum);
+    }
+
+    public static double calculateSum(Number... numbers) {
+        double result = 0;
+        for (Number num : numbers) {
+            result += num.doubleValue();
+        }
+        return result;
+    }
+
     public static void main(String[] args)
     {
         task1();
+        task2();
     }
 }
